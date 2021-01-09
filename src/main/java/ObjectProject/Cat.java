76da5c207652fec584bfd25414a407e18612e44c
lixiaochen <lixiaochen@meishubao.com>
2021-01-09 17:56:40 +0800

    构造函数和this关键字

--- src/main/java/ObjectProject/Cat.java
+++ src/main/java/ObjectProject/Cat.java
@@ -7,6 +7,18 @@ public class Cat {
     public double weigeht;
     public String species;
     //方法跑动、吃东西
+    public Cat(){
+        System.out.println("我是无参构造方法");
+    }
+    public Cat(String name){
+        System.out.println("我是带参构造方法");
+    }
+    public Cat(String name,int month,double weigeht, String species){
+        this.name = name;
+        this.month=month;
+        this.weigeht = weigeht;
+        this.species =species;
+    }
     public void run(String name){
         System.out.println(name + "小猫会跑");
     }
--- src/test/java/ObjectProjectTest/CatTest.java
+++ src/test/java/ObjectProjectTest/CatTest.java
@@ -8,11 +8,11 @@ public class CatTest {
         //one.name在没有赋值的时候，string 会有默认值null,int类型的默认值为0，double类型的默认值为0.0
         // Cat one 声明对象，是在栈中完成操作
         // new Cat() 实例化对象，是在堆里面完成的操作
-        Cat one = new Cat();
-        one.name="花花";
-        one.month=2;
-        one.species="英国短毛猫";
-        one.weigeht = 12.5;
+        Cat one = new Cat("花花",2,1000,"英国短毛猫");
+//        one.name="花花";
+//        one.month=2;
+//        one.species="英国短毛猫";
+//        one.weigeht = 12.5;
 
         one.run(one.name);
         one.eat();
